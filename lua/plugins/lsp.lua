43bd7f0a743e359d5884c7cf289c1214d3eb4c58
Mikhail Pecherkin <m.pecherkin.sas@gmail.com>
2025-01-24 14:31:10 +0500

    Fixed duplicate code diagnostics with Ruff/Pyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,14 @@
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
   --
   -- In this case, we create a function that lets us more easily define mappings specific
   -- for LSP related items. It sets the mode, buffer and description for us each time.
+  if client.name == 'ruff' then
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end
   local nmap = function(keys, func, desc)
     if desc then
       desc = 'LSP: ' .. desc
